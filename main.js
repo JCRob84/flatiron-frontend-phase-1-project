1bcd7e783a21c425230e293630a1a129fa426230
Juan Robledo <robledoc@gmail.com>
2023-08-03 15:22:54 -0500

    add fetch url, add shuffled countries slice

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const app = document.querySelector(".container");
 
 const url = "https://restcountries.com/v3.1/all";
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    const shuffledCountries = data.sort(() => Math.random() - 0.5);
+    const randomTenCountries = shuffledCountries.slice(0, 10);
+  });
