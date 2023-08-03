08ba316093d1333ca986bab8555f57a14e2d9640
Juan Robledo <robledoc@gmail.com>
2023-08-03 15:16:05 -0500

    change new url https://restcountries.com/v3.1/all

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-console.log("main.js connected");
+const app = document.querySelector(".container");
 
-const handleFormInputFocus = () => {
-  console.log(`focus occurred`);
-};
-
-const searchTermsInput = document.body.querySelector("#search-terms");
-
-searchTermsInput.addEventListener("focus", handleFormInputFocus);
+const url = "https://restcountries.com/v3.1/all";
