b19d4ab3313b87b46528e38be1757a22929cd7ce
Juan Robledo <robledoc@gmail.com>
2023-08-03 15:28:47 -0500

    add forEach country name, flag, population, laguanges

--- main.js
+++ main.js
@@ -7,4 +7,11 @@ fetch(url)
   .then((data) => {
     const shuffledCountries = data.sort(() => Math.random() - 0.5);
     const randomTenCountries = shuffledCountries.slice(0, 10);
+
+    randomTenCountries.forEach((country) => {
+      const name = country.name.common;
+      const flagUrl = country.flags.png;
+      const population = country.population;
+      const languages = Object.values(country.languages).join(", ");
+    });
   });
