0b7689dc17eeb68cae2be000f31be253d79f8e3e
Juan Robledo <robledoc@gmail.com>
2023-08-06 20:07:56 -0500

    create capital element

--- main.js
+++ main.js
@@ -34,4 +34,34 @@ document.addEventListener("DOMContentLoaded", () => {
             countryDiv.classList.add("country");
             countryDiv.id = country.cca3;
 
-          
+            const nameElement = document.createElement("p");
+            nameElement.textContent = `Country: ${country.name.common}`;
+            countryDiv.appendChild(nameElement);
+
+            const flagImg = document.createElement("img");
+            flagImg.src = country.flags.png;
+            flagImg.alt = `${country.name.common} flag`;
+            countryDiv.appendChild(flagImg);
+
+            const capitalElement = document.createElement("p");
+            capitalElement.textContent = `Capital: ${country.capital}`;
+            countryDiv.appendChild(capitalElement);
+
+            const populationElement = document.createElement("p");
+            populationElement.textContent = `Population: ${country.population}`;
+            countryDiv.appendChild(populationElement);
+
+            const languages = Object.values(country.languages).join(", ");
+            const languagesElement = document.createElement("p");
+            languagesElement.textContent = `Languages: ${languages}`;
+            countryDiv.appendChild(languagesElement);
+
+            countriesContainer.appendChild(countryDiv);
+          });
+        })
+        .catch((err) => console.log(err));
+    }
+  });
+
+  // Resto del código...
+});
